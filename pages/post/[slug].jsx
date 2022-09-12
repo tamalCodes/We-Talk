cbec18db0152038be9814d59e5910b45bb777e3c
Tamal Das <gyansujan69@gmail.com>
2022-09-12 12:14:21 +0530

    removing components

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -1,64 +1,48 @@
-import React, { useEffect } from 'react';
-import { PostCard, Categories, PostWidget, Header, Author, CommentsForm, Comments } from "../../components";
-import { getPosts, getPostDetails } from "../../services";
-import { useRouter } from 'next/router'
-import Head from 'next/head';
+import React from 'react';
+import { useRouter } from 'next/router';
 
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader, DetailedPost } from '../../components';
+import { getPosts, getPostDetails } from '../../services';
 
 const PostDetails = ({ post }) => {
-    const router = useRouter()
-    const { slug } = router.query
+    const router = useRouter();
 
-    // useEffect(() => {
-    //     console.log(post);
-    // }, [])
-
+    if (router.isFallback) {
+        return <Loader />;
+    }
 
     return (
-        <div className="container mx-auto px-10 mb-8">
-            <Head>
-                <title>{`Post | ${post.post.title}`}</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                {/* //* Centered posts */}
-
-                <div className="lg:col-span-8 col-span-1">
-                    <PostCard post={post.post} />
-                    <Author />
-                    <CommentsForm />
-                    <Comments />
-                </div>
-
-                {/* //* Right side posts */}
-                <div className="lg:col-span-4 col-span-1">
-                    {/* //* Parent for recent posts, categories  */}
-                    <div className="lg:sticky relative top-8">
-                        <PostWidget />
-                        <Categories />
+        <>
+            <div className="container mx-auto px-10 mb-8">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                    <div className="col-span-1 lg:col-span-8">
+                        <DetailedPost post={post} />
+                        <Author author={post.author} />
+                        <CommentsForm slug={post.slug} />
+                        <Comments slug={post.slug} />
+                    </div>
+                    <div className="col-span-1 lg:col-span-4">
+                        <div className="relative lg:sticky top-8">
+                            {/* <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} /> */}
+                            <Categories />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 export default PostDetails;
 
-export async function getStaticProps(context) {
-    const { slug } = context.params;
-    const post = await getPostDetails(slug);
-    console.log(post);
-
-    //? By returning { props: posts }, the Blog component will receive `posts` as a prop at build time
-
+// Fetch data at build time
+export async function getStaticProps({ params }) {
+    const data = await getPostDetails(params.slug);
     return {
         props: {
-            post: JSON.parse(JSON.stringify(post)),
-        }, // will be passed to the page component as props
-    }
+            post: data,
+        },
+    };
 }
-
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
@@ -69,6 +53,3 @@ export async function getStaticPaths() {
         fallback: true,
     };
 }
-
-
-
